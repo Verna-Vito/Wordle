86953a8258349ba29db0ce1430d90ebc02051891
Vito Verna <100713762+Verna-Vito@users.noreply.github.com>
2025-02-14 00:57:39 +0100

    Initial commit

---
+++ src/main/java/it/uniba/app/App.java
@@ -0,0 +1,13 @@
+package it.uniba.app;
+
+public final class App {
+
+    /**
+     * Inizio dell'applicazione.
+     *
+     * @param args - argomenti da linea di comando
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello my brother!");
+    }
+}
---
+++ src/test/java/it/uniba/app/AppTest.java
@@ -0,0 +1,8 @@
+package it.uniba.app;
+
+/**
+ * Main test class of the application.
+ */
+public class AppTest {
+
+}
